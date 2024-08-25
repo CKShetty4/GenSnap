c599e1a58d0ea6f590a5d6c915ce92c71c1bc141
CKShetty4 <ckshetty004@gmail.com>
2024-08-25 22:08:09 +0530

    Loaded Assets and Fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const index = () => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-5xl font-pblack">index</Text>
+      <Text className="text-3xl font-pblack">index</Text>
       <Text>Checking if this thing works</Text>
     </View>
   )
