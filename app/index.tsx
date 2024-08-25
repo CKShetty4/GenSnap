95537e32860d229844a51013b66ae85a8b2ff10d
CKShetty4 <ckshetty004@gmail.com>
2024-08-26 00:56:43 +0530

    Tabs added

--- app/index.tsx
+++ app/index.tsx
@@ -7,7 +7,7 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-5xl font-pblack">GenSnap</Text>
       <StatusBar style="auto" />
-      <Link href="/home" style={{color:'blue'}}>Go to Home</Link>
+      <Link href={{ pathname: "/home" }} style={{ color: 'blue' }}> Go to Home</Link>
     </View>
   );
 }
