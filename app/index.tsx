6af51a101676af19e19daff92152e5fa588046f8
CKShetty4 <ckshetty004@gmail.com>
2024-08-26 05:45:52 +0530

    Built first component and Welcome Screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,35 @@
 import { StatusBar } from "expo-status-bar";
-import { View, Text } from "react-native";
-import { Link } from "expo-router";
+import { ScrollView, View,Text,Image } from "react-native";
+import { router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {images} from '../constants';
+import CustomButton from "@/components/CustomButton";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-5xl font-pblack">GenSnap</Text>
-      <StatusBar style="auto" />
-      <Link href={{ pathname: "/home" }} style={{ color: 'blue' }}> Go to Home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{height:'100%'}}>
+<View className="w-full justify-center items-center h-full px-4">
+<Image source={images.logo} className="w-[130px] h-[84px]" resizeMode="contain"/>
+<Image source={images.cards} className="max-w-[380px] w-full h-[300px]" resizeMode="contain"/>
+<View className="relative mt-5">
+<Text className="text-3xl text-white font-bold text-center">Discover Endless posibilities with {' '}
+  <Text className="text-secondary-200">GenSnap</Text>
+</Text>
+<Image 
+source={images.path}
+className="w-[140px] h-[15] absolute -bottom-2 -right-2" resizeMode="contain"/>
+
+</View>
+<Text className="text-gray-100 mt-7 text-sm font-pregular text-center">Where creativity meets innovation: embark on a journey of limitless exploration with GenSnap</Text>
+<CustomButton
+  title="Continue with Email"
+  handlePress={()=>router.push('/sign-in')}
+  containerStyles="w-full mt-7"/>
+</View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" 
+      style='light'/>
+    </SafeAreaView>
   );
 }
