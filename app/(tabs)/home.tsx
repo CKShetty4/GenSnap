84e033dacd2fba0e1c1185d7289167c7e8067e93
CKShetty4 <ckshetty004@gmail.com>
2024-08-28 23:51:24 +0530

    Search Bar UI  Inserted

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,36 +1,47 @@
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, Image } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '../../constants'
+import SearchInput from '@/components/SearchInput'
 
 const Home = () => {
   return (
     <SafeAreaView className='bg-primary'>
       <FlatList
-      data={[{id:1},{id:2},{id:3},]}//Later the video data will be used here
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={({item}) => (
-        <View>
-          <Text className='text-3xl text-white'>{item.id}
-          </Text>
-        </View>
-      )}
-ListHeaderComponent={() => (
-  <View className='my-6 px-4 space-y-6'>
-    <View className='flex-row mb-6 justify-between items-start'>
-      <View>
-      <Text className='font-pmedium text-sm text-gray-100 ' >Welcome Back</Text>
-      <Text className='text-2xl font-psemibold text-white'>
-        CKShetty
-      </Text>
-      </View>
-      <View className=''>
+        data={[{ id: 1 }, { id: 2 }, { id: 3 },]}//Later the video data will be used here
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View>
+            <Text className='text-3xl text-white'>{item.id}
+            </Text>
+          </View>
+        )}
+        ListHeaderComponent={() => (
+          <View className='flex my-6 px-4 space-y-6 '>
+            <View className=' flex-row justify-between items-center mb-6'>
+              <View >
+                <Text className='font-pmedium text-sm text-gray-100 ' >Welcome Back</Text>
+                <Text className='text-2xl font-psemibold text-white'>
+                  CKShetty
+                </Text>
+              </View>
+              <View className='mt-1.5 flex-row'>
+                <Image
+                  source={images.logoSmall}
+                  className="w-8 h-16"
+                  resizeMode='contain' />
+              </View>
 
-      </View>
-      
-      </View>
-    
-  </View>
-)}
+            </View>
+<SearchInput/>
+<View className='w-full flex-1 pt-5 pb-8'>
+  {/* Latest Video Section */}
+<Text className='text-gray-100 text-lg font-pregular mb-3'>
+  Latest Videos
+</Text>
+</View>
+          </View>
+        )}
 
       />
     </SafeAreaView>
