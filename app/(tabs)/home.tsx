6ee0cf773d91c1a2f3ec8f523d3e38ad75366291
CKShetty4 <ckshetty004@gmail.com>
2024-09-02 23:23:23 +0530

    Trending posts or latest posts setup done

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -5,7 +5,7 @@ import { images } from '../../constants'
 import SearchInput from '@/components/SearchInput'
 import Trending from '@/components/Trending'
 import EmptyState from '@/components/EmptyState'
-import { getAllPosts } from '@/lib/appwrite'
+import { getAllPosts, getLatestPosts } from '@/lib/appwrite'
 import useAppwrite from '@/lib/useAppwrite'
 import VideoCard from '@/components/VideoCard'
 
@@ -14,6 +14,7 @@ import VideoCard from '@/components/VideoCard'
 const Home = () => {
 
 const {data:posts,refetch}=useAppwrite(getAllPosts);
+const {data:latestPosts}=useAppwrite(getLatestPosts);
 
 const [refreshng, setrefreshng] = useState(false)
 
@@ -58,9 +59,9 @@ console.log(posts)
 <View className='w-full flex-1 pt-5 pb-8'>
   {/* Latest Video Section */}
 <Text className='text-gray-100 text-lg font-pregular mb-3'>
-  Trending Videos
+  Latest Videos
 </Text>
-<Trending posts={[{id:1},{id:2},{id:3}]??[]}/>
+<Trending posts={latestPosts??[]}/>
 </View>
           </View>
         )}
--- components/Trending.tsx
+++ components/Trending.tsx
@@ -1,18 +1,63 @@
-import { View, Text, FlatList } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import * as Animatable from 'react-native-animatable'
+
+const ZoomIn={
+  0:{
+    scale:0.9
+  },
+  1:{
+    scale:1
+  }
+}
+
+const ZoomOut={
+  0:{
+    scale:1
+  },
+  1:{
+    scale:0.9
+  }
+}
+
+
+const TrendingItem = ({activeItem, item}: {activeItem: any, item: any}) => {
+  const [play, setplay] = useState(false)
+  return (
+    <Animatable.View 
+    className='mr-5'
+    //animation={activeItem===item.$id ? ZoomIn : ZoomOut }
+    animation={activeItem===item.$id ? JSON.stringify(ZoomIn) : JSON.stringify(ZoomOut)}
+    duration={500}
+    >
+      {play?(<Text>Playing</Text>):(<TouchableOpacity/>)}
+    </Animatable.View>
+  )
+}
 
 interface Post {
-  id: number
+  $id: string;
+  title: string;
+  thumbnail: string;
+  video: string;
+  creator: {
+    username: string;
+    avatar: string;
+  };
 }
 
 const Trending = ({ posts }: { posts: Post[] }) => {
+const [activeItem, setactiveItem] = useState(posts[0]);
+
   return (
     <FlatList
       data={posts}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item }) => (
-        <Text className='text-white text-3xl'>{item.id}</Text>
-      )}
+      keyExtractor={(item: { $id: string }) => item.$id}//(item) => item.$id
+        renderItem={({ item }: { item: { $id: string; title: string; thumbnail: string; video: string; creator: { username: string; avatar: string; } } }) => (
+          // renderItem={({ item }) => (
+          <TrendingItem
+          activeItem={}
+          />)}
       horizontal
     />
   )
