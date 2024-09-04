4355d1e02b494adbbc595a1a9b99d342a9ab2851
CKShetty4 <ckshetty004@gmail.com>
2024-09-04 21:08:10 +0530

    Search Bar Implementation

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,7 +1,8 @@
-import { View, Text, TextInput ,TouchableOpacity, Image} from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 
-import {icons} from '../constants';
+import { icons } from '../constants';
+import { router, usePathname } from 'expo-router';
 
 // interface FormFieldProps {
 //     title: string;
@@ -11,22 +12,15 @@ import {icons} from '../constants';
 //     otherStyles?: string;
 //     keyboardType?:string;
 //   }
-  
+
 //   const SearchInput: React.FC<FormFieldProps> = ({
 
-    const SearchInput = ({
-    // title,
-    // value,
-    // placeholder,
-    // handleChangeText,
-    // otherStyles,
-    // ...props
-  }) => {
-const [showPassword, setshowPassword] = useState(false)
-    
+const SearchInput = () => {
+  const pathname = usePathname();
+  const [query, setQuery] = useState('')
   return (
-    
-      <View 
+
+    <View
       className="border-2
        border-black-200 
        w-full h-16 px-4
@@ -36,18 +30,28 @@ const [showPassword, setshowPassword] = useState(false)
          items-center
           focus:border-secondary
           space-x-4">
-<TextInput className="flex-1 text-white font-pregular text-base mt-0.5 "
-// value={value}
- placeholder="Search for a video topic"
-placeholderTextColor="#7b7b8b"
-// onChangeText={handleChangeText}
-// secureTextEntry={title === 'Password' && !showPassword}
-/>
-
-<TouchableOpacity>
-    <Image source={icons.search} className="w-5 h-5" resizeMode="contain"/>
-</TouchableOpacity>
-      </View>
+      <TextInput className="flex-1 text-white font-pregular text-base mt-0.5 "
+        value={query}
+        placeholder="Search for a video topic"
+        placeholderTextColor="#CDCDE0"
+      onChangeText={(e)=>setQuery(e)}
+      />
+      <TouchableOpacity
+      onPress={() => {
+        if (!query){
+          return Alert.alert('Missing Query', 'Please input something to search results')
+        }
+        if(pathname.startsWith('/search')){
+          router.setParams({query})
+        }
+        else{
+          router.push(`/search/${query}`)
+        }
+      }}
+      >
+        <Image source={icons.search} className="w-5 h-5" resizeMode="contain" />
+      </TouchableOpacity>
+    </View>
   )
 }
 
